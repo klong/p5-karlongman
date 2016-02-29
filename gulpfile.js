e2698a0290803f948e00f770c71a62390c5dbc9f
karl longman <karl_longman@hotmail.com>
2016-02-29 00:40:58 +0000

    basic google map

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,44 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var webserver = require('gulp-webserver');
 var watch = require('gulp-watch');
+var imagemin = require('gulp-imagemin');
+var pnqquant = require('imagemin-pngquant');
+var cache = require('gulp-cache');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+//var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 
 // PATHS
 var pathTo = {
-  'app': ['src/app/'],
-  'dist': ['dist'],
-  'html': ['src/app/*.html'],
-  'scripts': ['src/app/js/**/*.js'],
-  'styles': ['src/app/css/**/*.css']
+  'app':      'src/app/',
+  'dist':     'dist',
+  'html':     'src/app/*.html',
+  'imgSrc':   'src/img/**/*.+(jpeg|jpg|png)',
+  'images':   'src/app/img/',
+  'scripts':  'src/app/js/**/*.js',
+  'styles':   'src/app/css/**/*.css'
+};
+
+var _gulpsrc = gulp.src;
+gulp.src = function() {
+    return _gulpsrc.apply(gulp, arguments)
+    .pipe(plumber({
+        errorHandler: function(err) {
+            notify.onError({
+                title:    "Gulp Error",
+                message:  "Error: <%= error.message %>",
+            })(err);
+            this.emit('end');
+        }
+    }));
 };
 
 gulp.task('webserver', function() {
-  gulp.src(pathTo.app)
+  return gulp.src(pathTo.app)
     .pipe(webserver({
       livereload: true,
       //directoryListing: true,
@@ -21,27 +47,34 @@ gulp.task('webserver', function() {
     }));
 });
 
+gulp.task('clear', function (done) {
+  return cache.clearAll(done);
+});
+
+gulp.task('images', function() {
+  return gulp.src(pathTo.imgSrc)
+    // cache images so only images not already in src/img/.. are processed
+    .pipe(cache(imagemin({
+      progressive: true,
+      svgoPlugins: [
+          {removeViewBox: false},
+          {cleanupIDs: false}
+      ],
+      use: [pnqquant()]
+    })))
+    .pipe(gulp.dest(pathTo.images));
+});
+
 gulp.task('html', function() {
-  gulp.src(pathTo.html);
-  //.pipe(connect.reload());
-  console.log('html task..');
+  return gulp.src(pathTo.html);
 });
 
 gulp.task('scripts', function() {
-  gulp.src(pathTo.scripts);
-  console.log('scripts task..');
+  return gulp.src(pathTo.scripts);
 });
 
 gulp.task('styles', function() {
-  gulp.src(pathTo.styles);
-  console.log('styles task..');
-});
-
-
-gulp.task('watch', function() {
-  gulp.watch(pathTo.html, ['html']);
-  gulp.watch(pathTo.scripts, ['scripts']);
-  gulp.watch(pathTo.styles, ['styles']);
+  return gulp.src(pathTo.styles);
 });
 
 // Removes all files from ./dist/
@@ -50,6 +83,13 @@ gulp.task('clean', function() {
       read: false
     })
     .pipe(clean());
+});
+
+// watch task
+gulp.task('watch', function() {
+  gulp.watch(pathTo.html, ['html']);
+  gulp.watch(pathTo.scripts, ['scripts']);
+  gulp.watch(pathTo.styles, ['styles']);
 });
 
 // Default Task
--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,1 +1,54 @@
-// sdasda
+// app.js
+
+// googleMapas API callback function
+
+function initMap() {
+
+  var uluru = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: uluru,
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "rssp"
+  });
+
+  var markers = [];
+
+  var goldStar = {
+   path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+   fillColor: 'yellow',
+   fillOpacity: 0.8,
+   scale: 0.1,
+   strokeColor: 'red',
+   strokeWeight: 2
+ };
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: 'Uluru (Ayers Rock)',
+    icon: goldStar
+  });
+
+  markers.push(marker);
+
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+}
